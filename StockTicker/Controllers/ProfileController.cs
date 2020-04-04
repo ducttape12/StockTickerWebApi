1a0bf7e04ab1a43086b13f57a74ad947640ee32b
Keith Ott <ducttape12@yahoo.com>
2020-04-04 14:40:50 -0500

    Enabled CORS for local development

--- StockTicker/Controllers/ProfileController.cs
+++ StockTicker/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using StockTicker.Interfaces;
 using StockTicker.Models;
 
--- StockTicker/Startup.cs
+++ StockTicker/Startup.cs
@@ -20,6 +20,16 @@ namespace StockTicker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                        .WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
             services.AddControllers();
             services.AddTransient<IFinancialModelingPrepClient, FinancialModelingPrepClient>();
         }
@@ -27,6 +37,8 @@ namespace StockTicker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
